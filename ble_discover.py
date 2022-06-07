135e4f8fe59f9590ae15f23533309790fbb15ab3
Luca Ghislotti <l.ghislotti@studenti.unibg.it>
2022-06-07 11:25:25 +0200

    Now working with logs

--- ble_discover.py
+++ ble_discover.py
@@ -3,7 +3,7 @@ import time
 import datetime
 from bleak import BleakClient
 
-address = "CE:69:45:B5:8A:46" # change to winter_24 MAC
+address = "CE:69:45:B5:8A:46" #winter_24 MAC
 HUMIDITY_SERVICE_UUID = "00002a6f-0000-1000-8000-00805f9b34fb"
 TEMPERATURE_SERVICE_UUID = "00002a6e-0000-1000-8000-00805f9b34fb"
 PRESSURE_SERVICE_UUID = "00002a6d-0000-1000-8000-00805f9b34fb"
@@ -27,21 +27,21 @@ async def main(address):
 
                 f = open("humidity_readings.txt", "a")
 
-                print(now)
+                print("\n" + str(now))
                 print("   humidity: " + str(humidity_int/100) + " %")
                 print("temperature: " + str(temperature_int/100) + " °C")
                 print("   pressure: " + str(pressure_int/1000) + " mBar")
 
                 if(battery_int == 102):
-                    print("    battery: cable\n")
+                    print("    battery: cable")
                 else:
-                    print("    battery: " + str(battery_int) + " %\n")
+                    print("    battery: " + str(battery_int) + " %")
 
                 f.write(str(now.year) + "\t" + str(now.month) + "\t" + str(now.day) + "\t" + str(now.hour) + "\t" + str(now.minute) + "\t" + str(now.second) + "\t" + str(humidity_int) + "\t" + str(temperature_int) + "\t" + str(pressure_int) + "\t" + str(battery_int) + "\n")
 
                 f.close()
                 
         except:
-            print("winter_GAPS not available")
+            print("\nwinter_GAPS not available")
 
 asyncio.run(main(address))
