a045424d03cdd4c8759d33c5634ac0909887f196
Luca Ghislotti <l.ghislotti@studenti.unibg.it>
2022-06-07 18:18:54 +0200

    added plot

--- plot.py
+++ plot.py
@@ -1,21 +1,41 @@
-# importing the required module
-import matplotlib.pyplot as plt
-  
-# x axis values
-x = [1,2,3]
-# corresponding y axis values
-y = [2,4,1]
-  
-# plotting the points 
-plt.plot(x, y)
-  
-# naming the x axis
-plt.xlabel('x - axis')
-# naming the y axis
-plt.ylabel('y - axis')
-  
-# giving a title to my graph
-plt.title('My first graph!')
-  
-# function to show the plot
-plt.show()
+import serial
+from tkinter import *
+from matplotlib import pyplot as plt
+
+Top = Tk()
+
+x = []
+y = []
+func_id = None
+
+def read_data():
+    global func_id
+    plt.ion()
+    new_value = ser.readline().decode('ascii')
+    if new_value == '':
+        pass
+    else:
+        y.append(eval(new_value[:-2]))
+        x.append(len(y) - 1)
+        plt.plot(x, y, 'r-')
+        plt.show()
+        plt.pause(0.0001)
+    func_id = Top.after(100, read_data)
+
+def close_plot():
+    global func_id
+    #to no longer update the plot
+    Top.after_cancel(func_id)
+    plt.close()
+    global x, y
+    del x[:]
+    del y[:]
+
+def quit():
+    Top.destroy()
+
+Button(Top, text='Read', command=read_data).pack()
+Button(Top, text='Close plot', command=close_plot).pack()
+Button(Top, text='Quit', command=quit).pack()
+
+mainloop()
