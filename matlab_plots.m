8e38283fdaad426f5cd3078454353b79fd9cba9a
Luca Ghislotti <l.ghislotti@studenti.unibg.it>
2022-06-09 17:10:58 +0200

    Update

--- matlab_plots.m
+++ matlab_plots.m
@@ -4,12 +4,14 @@ clear; clc;
 
 DATA = readtable("data_readings.txt", "VariableNamingRule", "preserve");
 
+DATA = DATA(1:end, :);
+
 dataora = datetime(DATA.Var1, DATA.Var2, DATA.Var3, DATA.Var4, DATA.Var4, DATA.Var5, DATA.Var6);
 
 humidty = smoothdata(DATA.Var7/100);
 temperature = smoothdata(DATA.Var8/100);
 pressure = smoothdata(DATA.Var9/1000);
-battery = smoothdata(DATA.Var10);
+battery = DATA.Var10;
 
 f = figure(Visible="on");
 
@@ -17,20 +19,25 @@ f = figure(Visible="on");
 subplot(2, 2, 1);
 plot(dataora, humidty, 'LineWidth',1.5, 'Color', 'red');
 title('Humidity [%]');
+axis('padded')
+grid("on")
 
 % temperature [°C]
 subplot(2, 2, 2);
 plot(dataora, temperature, 'LineWidth',1.5, 'Color', 'green');
 title('Temperature [°C]');
+grid("on")
 
 % pressure [mBar]
 subplot(2, 2, 3);
 plot(dataora, pressure, 'LineWidth',1.5, 'Color', 'blue');
 title('Pressure [mBar]');
+grid("on")
 
 % battery [%]
 subplot(2, 2, 4);
 plot(dataora, battery, 'LineWidth',1.5, 'Color', 'magenta');
 title('Battery [%]');
+grid("on")
 
 sgtitle('Winter Enviroment Monitor');
