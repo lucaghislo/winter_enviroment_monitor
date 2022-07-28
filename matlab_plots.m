10cee4ae7fb9917f4d49ba2ca18c36ef0ab30dde
Luca Ghislotti <ghislottiluca@gmail.com>
2022-07-28 12:04:07 +0200

    Updated plot

--- matlab_plots.m
+++ matlab_plots.m
@@ -35,25 +35,31 @@ plot(dataora, humidty, 'LineWidth',1.5, 'Color', 'red');
 title('\textbf{Humidity [$\%$]}');
 axis('padded')
 grid("on")
+set(gca,'FontSize', 12)
 
 % temperature [°C]
 subplot(2, 2, 2);
 plot(dataora, temperature, 'LineWidth',1.5, 'Color', 'green');
 title('\textbf{Temperature [$^{\circ}$C]}');
 grid("on")
+set(gca,'FontSize', 12)
 
 % pressure [mBar]
 subplot(2, 2, 3);
 plot(dataora, pressure, 'LineWidth',1.5, 'Color', 'blue');
 title('\textbf{Pressure [mBar]}');
 grid("on")
+set(gca,'FontSize', 12)
 
 % battery [%]
 subplot(2, 2, 4);
 plot(dataora, battery, 'LineWidth',1.5, 'Color', 'magenta');
 title('\textbf{Battery [$\%$]}');
 grid("on")
+set(gca,'FontSize', 12)
 
 sgtitle('\textbf{GAPS Module Monitor}');
+set(gca,'FontSize', 12)
 
+f.Position = [0 0 1920  1080];
 exportgraphics(gcf,'winter_plot.pdf','ContentType','vector');
